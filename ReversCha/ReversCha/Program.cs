e06d1c010311f038dc8089d4a4e123eb78a1a52b
Sudorson <s.krisna.d@gmail.com>
2019-01-10 19:08:37 +0600

    ReverseCharacters

---
+++ ReversCha/ReversCha/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SamplePrograms
+{
+    class ReverseCharacters
+    {
+        public static void Main()
+        {
+            // Prompt the user to enter the string
+            Console.WriteLine("Please enter your string");
+
+
+            // Read the user string from console
+            string UserString = Console.ReadLine();
+
+
+            // The simple way to reverse a string is to use
+            // the built-in .net framework Reverse() function
+            List<char> StringCharacters = UserString.Reverse().ToList();
+
+            // Finally print each character from the collection
+            foreach (char c in StringCharacters)
+            {
+                Console.Write(c);
+            }
+
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
