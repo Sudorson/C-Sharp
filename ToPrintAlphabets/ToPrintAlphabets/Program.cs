3456010ff781a23038fe55241eb9f03473842151
Sudorson <s.krisna.d@gmail.com>
2019-01-10 10:16:51 +0600

    PrintAlphabets

---
+++ ToPrintAlphabets/ToPrintAlphabets/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace SamplePrograms
+{
+    class Alphabets
+    {
+        public static void Main()
+        {
+            for (char i ='a'; i <= 'z'; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            for (char i = 'A'; i <= 'Z'; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
