45c3da8cb074c7a1d3f688abab8ef8c4e3aefc44
Sudorson <s.krisna.d@gmail.com>
2019-01-10 20:07:34 +0600

    find Missing number from Array

---
+++ Exercise/MissingNumber/MissingNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissingNumber
+{
+
+    class MissingNo
+    {
+        public void MissingArray(int[] intArray)
+        {
+            Array.Sort(intArray);
+            for (int i = 0; i < intArray.Length - 1; i++)
+            {
+                int j = (intArray[i + 1] - intArray[i]) - 1;
+                if (j >= 1)
+                {
+                    while (j != 0)
+                    {
+                        Console.Write(++intArray[i]+" ");
+                        j--;
+                    }
+                }
+
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MissingNo missing = new MissingNo();
+            missing.MissingArray(new int[] { 1, 5, 9 });
+            Console.ReadKey();
+        }
+
+    }
+
+   
+}
