b4fa4c479a7fbcd58e9e0dbfeb89d469bd9840df
Sudorson <s.krisna.d@gmail.com>
2019-01-10 09:14:58 +0600

    Factorial

---
+++ Factorial/Factorial/Factorial/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number ");
+            int tangetNumber = Convert.ToInt32(Console.ReadLine());
+            if (tangetNumber==0)
+            {
+                Console.WriteLine("Factorial Is 1");
+            }
+            else if (tangetNumber<0)
+            {
+                Console.WriteLine("please enter a positive number");
+            }
+            else 
+            {
+                Double factorial = 1;
+
+                for (int i = tangetNumber; i >=1; i--)
+                {
+                    factorial = factorial * i;
+                }
+                Console.WriteLine("Factorial of {0}={1}", tangetNumber, factorial);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Find duplicates in array/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 10, 5, 10, 2, 2, 3, 4, 5, 5, 6, 7, 8, 9, 11, 12, 12 };
+            var dict = new Dictionary<int, int>();
+            foreach (var num in array)
+            {
+                if (!dict.ContainsKey(num))
+                {
+                    dict.Add(num, 0);
+                }
+                dict[num]++;
+            }
+
+            foreach (var kvp in dict)
+            {
+                Console.WriteLine("{0} repeats {1} times", kvp.Key, kvp.Value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
